883b8b55fca06845c65aeb43d70734820e08b1c2
Pearl Sai <pearlsai91@gmail.com>
2022-07-13 18:32:20 -0600

    change footer styling

--- footer.php
+++ footer.php
@@ -1,32 +1,39 @@
 <footer>
-<div class="footer-container">
-  <div class="footer">
-    <div class="footer-heading footer-1">
+<div class="contianer footer-container">
+  <div class="row footer">
+    <div class="col-lg-4 footer-1">
       <h3 class="footer-h3">LOCATION</h3>
       <p>Black Gold Mobile Oil Services <br>
          7724 N Secretariat RD, <br>
          Eagle Mountain UT, 84005</p>
     </div>
 
-    <div class="footer-heading footer-2">
+    <div class="col-lg-4 footer-2">
       <h3 class="footer-h3">CONTACT</h3>
       <p>Owner/Operator Chad Rasmusen</p>
         <p><strong>Phone:</strong><br>
          (801) 819-GOLD(4653)</p>
          <p><strong>Email:</strong></p>
-        <a href="mailto:blackgoldmobileiol@gmail.com">blackgoldmobileiol@gmail.com</a></p>
+        <a href="mailto:blackgoldmobileiol@gmail.com">blackgoldmobileiol@gmail.com</a>
     </div>
 
-    <div class="footer-heading footer-3">
+    <div class="col-lg-4 footer-3">
       <h3 class="footer-h3">QUICK LINKS</h3>
-
-      <a href="#Ammenities">Ammenities</a>
-      <a href="#Availability">Availability</a>
-      <a href="#Cancellation">Cancellation Policy</a>
+      <a href="#Ammenities">Ammenities</a><br>
+      <a href="#Availability">Availability</a><br<
+      <a href="#Cancellation">Cancellation Policy</a><br>
       <a href="#Location">Location</a>
     </div>
+    <div class='row'>
+      <div class='footer-icons'>
+      <a class='social-icon' href='https://www.facebook.com/BlackGoldMobileOil/'><i class="fa-brands fa-facebook"></i></a>
+      <a class='social-icon' href='https://www.yelp.com/biz/black-gold-mobile-oil-service-clearfield'><i class="fa-brands fa-yelp"></i></a>
+      <a class='social-icon' href=''><i class="fa-brands fa-instagram"></i></a>
+      <a class='social-icon' href='https://www.linkedin.com/in/chad-rasmusen-29827516/'><i class="fa-brands fa-linkedin"></i></a>
+      <p>© Copyright BlackGold 2022</p>
+      </div>
+      </div>
   </div>
-</div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
