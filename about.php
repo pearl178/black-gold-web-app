6f4ad9c8ea4545632ea40d1ca22133885f6ed584
Pearl Sai <pearlsai91@gmail.com>
2022-07-13 18:31:40 -0600

    add context to about page and change styling

--- about.php
+++ about.php
@@ -1,43 +1,54 @@
 <?php include('navbar.php'); ?>
-
-    <!----------------Body----------------->
-    <div class="pagebody-container">
-        <div class="pagebody-1">
-            <div class="pagebody-1-left">
-                <img src="img/logo.jpg" width=80%>
-            </div>
-            <div class="pagebody-1-right">
-                <h3>A one-liner introduction here</h3>
-            </div>
+<div class="pagebody-container">
+  <div class='container'>
+    <div class='row'>
+      <div class='col-lg-7'>
+    <div class="pagebody-1">
+        <div class="pagebody-1-left">
+            <img src="img/logo.jpg" width=80%>
         </div>
-        <div class="pagebody-new">
-            <div class="pagebody-new-left">
-                <h4>A brief introduction of the founder.</h4>
-            </div>
-            <div class="pagebody-new-right">
-                <h4>Founder Introduction</h4>
-                <img src="img/img2.JPG" width='100%'>
-                <h4>Chad Rasmusun</h4>
-            </div>
         </div>
-        <div class="pagebody-2">
-            <div class="pagebody-2-left">
-                <div class="working-pics">
-                    <div class="pictures">
-                        <img src="img/img2.JPG" width="80%">
-                    </div>
-                    <div class="pictures">
-                        <img src="img/img2.jpg" width="80%">
-                    </div>
-                    <div class="pictures">
-                        <img src="img/img2.jpg" width="80%">
-                    </div>
-                </div>
-                <h3>Our Team</h3>
-            </div>
-            <div class="pagebody-2-right">
-                <img src="img/img3.jpg" width=100% height=50%>
-            </div>
+        </div>
+        <div class='col-lg-5'>
+        <div class="pagebody-1-right">
+            <h4>Black Gold Mobile Oil Services is based out of Eagle Mountain UT.<br>
+              <br>
+               We service customers across the Wasatch Front in Utah. <br>
+               <br>
+               We specialize in mobile oil change & other light maintenance services for Automobiles, Commercial vehicles, UTV’s, Motorhomes, Boats and even your Grandfather’s tractor. </h4>
+        </div>
+        </div>
+        </div>
+        </div>
+    <div class="pagebody-new row">
+        <div class="pagebody-new-left col-lg-8">
+            <h4>Black Gold Mobile Oil Services was established in 2018 and has been a veteran owned business from Day 1.<br>
+               Chad Rasmusen acquired the business in late 2020. <br>
+               Chad is a 17 year Air Force Veteran.</h4>
+        </div>
+        <div class="pagebody-new-right col-lg-4">
+            <h2>Founder</h2>
+            <img src="img/owner-profile.jpeg" width='80%'>
+            <h4>Chad Rasmusen</h4>
         </div>
     </div>
+    <div class="pagebody-2">
+    <div class='container'>
+      <div class='row'>
+      <div class='col-lg-8'>
+        <div class="pagebody-2-left">
+            <h4>Our business Motto is “We are your personal pit crew.” <br>
+              We try to go above and beyond just changing the oil. <br>
+               We are methodical and take care to make sure that your vehicle is treated like it is our own personal vehicle or equipment.</h4>
+               <hr>
+            <h4>We are fully licensed and insured as an auto shop, so picking us for your next oil change, fuel filter change or service on your vehicle is always covered to make sure our customers are protected. </h4>
+        </div>
+        </div>
+        <div class='col-lg-4'>
+            <img src="img/img3.jpg" width=100% height=50%>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
 <?php include('footer.php'); ?>
