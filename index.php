3bb0b13354bc36e9cca21af9db197a747b14c3ed
Pearl Sai <pearlsai91@gmail.com>
2022-07-13 18:33:16 -0600

    change styling, make website responsive

--- index.php
+++ index.php
@@ -1,48 +1,169 @@
 <?php include('navbar.php'); ?>
 <div class="hero-image">
   <div class="hero-text">
-    <h1 style="font-size:50px">COMPLIMENTARY QUALITY SERVICES</h1>
-    <p class="main">Depth of tire tread - Clean windshield - Top off washer fluid</p>
+    <h1 style="font-size:50px">MOBILE OIL SERVICES</h1>
+    <h2 class="pit-crew">Your Personal Pit Crew</h2>
   </div>
 </div>
 <div class="padd">
-        <div class="back">
-        <h1 class="title">MOBILE OIL SERVICE</h1>
-            <p class="main"> Gas Engines Black Gold is proud to offer our customers 3 different CAR CARE PACKAGES. <br>
-            #1 (Black Package) Offers your vehicle a Mobil Synthetic Blend Oil &
-            a High Flow Mobil Filter. <br>
-            $65.00 +tax <br>
-            #2 (Silver Package) Offers your vehicle a Mobil 1 or Amsoil OE Full Synthetic Oil &
-            a Quality Synthetic High Flow Filter.
-            $85.00 +tax <br>
-            #3 (Gold Package) Offers your vehicle an Amsoil XL Boost 12,000 mile 100% Synthetic Oil &
-            a Quality Synthetic High Flow Oil Filter.
-            $115.00 +tax
-        </p><center>
-        <button class="button button1" type="button" onclick="window.location.href='schedule.php';">Schedule Service</button>
-        </center>
+  <div class="back">
+  <h1 class="title">GAS ENGINES</h1>
+  <p class="main">Black Gold is proud to offer our customers 3 different CAR CARE PACKAGES. </p>
+  <div class="row">
+  <div class='pricing-column col-lg-4 col-md-6'>
+    <div class="card">
+      <div class="card-header py-3">
+        <h2>1 Black Package</h2>
+      </div>
+      <div class="card-body">
+        <h3>$65.00 +tax</h3>
+        <h5>Mobil Synthetic Blend Oil</h5>
+        <h5>High Flow Mobil Filter</h5>
+        <p style='visibility:hidden'>Unlimited App Usage</p>
+      </div>
     </div>
-<div class="container">
-  <div class="image">
-    <img src="img/img2.JPG">
   </div>
-
-  <div class="text">
-    <center>
-     <h1 class="title-one">Complimentary Quality Services </h1>
-            <p class="main"> • Depth of tire tread <br>
-                             • Clean windshield <br>
-                             • Top off washer fluid
-             </p>
-    </center>
+  <div class='pricing-column col-lg-4 col-md-6'>
+    <div class="card">
+      <div class="card-header py-3">
+        <h2>2 Silver Package</h2>
+      </div>
+      <div class="card-body">
+        <h3>$85.00 +tax</h3>
+        <h5>Mobil 1 or Amsoil OE Full Synthetic Oil</h5>
+        <h5>Quality Synthetic High Flow Filter</h5>
+        <p style='visibility:hidden'>Unlimited App Usage</p>
+      </div>
+    </div>
   </div>
+  <div class='pricing-column col-lg-4'>
+    <div class="card">
+      <div class="card-header py-3">
+        <h2>3 Gold Package</h2>
+      </div>
+      <div class="card-body">
+        <h3>$115.00 +tax</h3>
+        <h5>Amsoil XL Boost 12,000 mile 100% Synthetic Oil</h5>
+        <h5>Quality Synthetic High Flow Oil Filter</h5>
+      </div>
+    </div>
+  </div>
+  <p class="main"> (All Packages Include Up to 5 Qts/ Additional Qt pricing listed below) <br>
+    Black Package - $6.99/ Qt +tax <br>Silver Package - $8.99/ Qt +tax <br>Gold Package - $12.99/ Qt +tx Diesel Engines<p>
 </div>
-     <h1 class="title">Your Personal Pit Crew</h1>
-            <p class="main-two"> (All Packages Include Up to 5 Qts/ Additional Qt pricing listed below) <br>
-                Black Package - $6.99/ Qt +tax Silver Package - $8.99/ Qt +tax Gold Package - $12.99/ Qt +tx Diesel Engines <br>
-            #4 (Diesel) Mobil Delvac 1300 Synthetic. $115.00 +tax <br>
-            #5 (Diesel) Amsoil Heavy Duty Synthetic.
-            $180.00 +tax (All Packages include up to 10 Qts & a Full Synthetic High Flow Oil Filter) Additional Mobil 1 Qts are $7.99 +tax Additional Amsoil Qts are $11.99 +tax
-        </p>
+  <center>
+  <button class="button button1" type="button" onclick="window.location.href='schedule.php';">Schedule Service</button>
+  </center>
+</div>
+<div class="container">
+
+
+  <section class='complements'>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class='container'>
+            <div class="row">
+              <div class="col-lg-6">
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Depth of tire tread</h3>
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Check air filter</h3>
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Inspect wiper blades</h3>
+              </div>
+              <div class="col-lg-6">
+                <img src='img2.JPG' class='complement-img'>
+              </div>
+          </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class='container'>
+            <div class="row">
+              <div class="col-lg-6">
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Clean windshield</h3>
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Check tire pressure</h3>
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Visually inspect that your vehicle has not leaks on major components</h3>
+              </div>
+              <div class="col-lg-6">
+                <img src='img2.JPG' class='complement-img'>
+              </div>
+          </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class='container'>
+            <div class="row">
+              <div class="col-lg-6">
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Top off washer fluid</h3>
+                <i class="fa-solid fa-circle-check complement-icons"></i>
+                <h3 class='feature-title'>Vaccum front interior</h3>
+              </div>
+        <div class="col-lg-6">
+          <img src='img2.JPG' class='complement-img'>
+        </div>
     </div>
+    </div>
+  </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    </section>
+</div>
+<div class=diesel>
+     <h1 class="title">DIESEL ENGINES</h1>
+     <div class="row">
+     <div class='pricing-column col-lg-6'>
+       <div class="card">
+         <div class="card-header py-3">
+           <h2>4 Diesel</h2>
+         </div>
+         <div class="card-body">
+           <h3>$115.00 +tax</h3>
+           <h5>Mobil Delvac 1300 Synthetic.</h5>
+           <h5 style='visibility:hidden'>High Flow Mobil Filter</h5>
+           <p style='visibility:hidden'>Unlimited App Usage</p>
+         </div>
+       </div>
+     </div>
+     <div class='pricing-column col-lg-6'>
+       <div class="card">
+         <div class="card-header py-3">
+           <h2>5 Diesel</h2>
+         </div>
+         <div class="card-body">
+           <h3>$180.00 +tax</h3>
+           <h5>Amsoil Heavy Duty Synthetic</h5>
+           <h5>Quality Synthetic High Flow Filter</h5>
+           <p style='visibility:hidden'>Unlimited App Usage</p>
+         </div>
+       </div>
+     </div>
+     </div>
+     <p class="main"> (All Packages include up to 10 Qts & a Full Synthetic High Flow Oil Filter) <br>
+       Additional Mobil 1 Qts - $7.99 +tax <br>Additional Amsoil Qts - $11.99 +tax<p>
+    <div class='additional'>
+      <h1 class="title">ADDITIONAL QUALITY SERVICES</h1>
+      <h4>Windowshield Wiper Replacement</h4>
+      <h5>$12.00 per wipe + tax</h5>
+      <h4>Tire Rotation</h4>
+      <h5>$125.00 (all 4 tires)</h5>
+      <h4>Air Filter Replacement</h4>
+      <h5>Starting at $20.00 + tax</h5>
+    </div>
+     </div>
+    </div>
+
 <?php include('footer.php'); ?>
